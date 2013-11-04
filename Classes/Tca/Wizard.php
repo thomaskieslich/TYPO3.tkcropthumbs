b93705beaaf84ff71a5c9fb0c4d610455b880a66
Thomas Kieslich <info@thomaskieslich.net>
2013-11-04 12:30:30 +0100

    - remove locallang.xml

--- Classes/Tca/Wizard.php
+++ Classes/Tca/Wizard.php
@@ -1,35 +1,42 @@
 <?php
 namespace ThomasKieslich\Tkcropthumbs\Tca;
-/**
- * Created by PhpStorm.
- * User: Thomas
- * Date: 02.11.13
- * Time: 15:40
- */
+
+	/***************************************************************
+	 *  Copyright notice
+	 *
+	 *  (c) 2013 Thomas Kieslich
+	 *  All rights reserved
+	 *
+	 *  This script is part of the TYPO3 project. The TYPO3 project is
+	 *  free software; you can redistribute it and/or modify
+	 *  it under the terms of the GNU General Public License as published by
+	 *  the Free Software Foundation; either version 3 of the License, or
+	 *  (at your option) any later version.
+	 *
+	 *  The GNU General Public License can be found at
+	 *  http://www.gnu.org/copyleft/gpl.html.
+	 *
+	 *  This script is distributed in the hope that it will be useful,
+	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *  GNU General Public License for more details.
+	 *
+	 *  This copyright notice MUST APPEAR in all copies of the script!
+	 ***************************************************************/
 
 /**
  * Class Wizard
- *
- * @package ThomasKieslich\Tkcropthumbs\Tca
  */
 class Wizard {
+
 	public function showIcon($fObj) {
-//		\TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($fObj['row']['tx_tkcropthumbs_crop']);
 		$iconPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('tkcropthumbs') . 'Resources/Public/Icons';
 		$formField = '<a href="#"  onclick="window.open(\'';
 		$formField .= 'mod.php?M=user_TkcropthumbsCrop&image=' . $fObj['row']['uid'];
-		$formField .= '\');">';
+		$formField .= '\',\'tkcropthumbs' . rand(0, 1000000) . '';
+		$formField .= '\',\'height=620,width=820,status=0,menubar=0,scrollbars=0\');return false;">';
 		$formField .= '<img src="' . $iconPath . '/crop.png">';
 		$formField .= '</a>';
-//		$return = '<a href="#"  onclick="window.open(\''
-//			. 'mod.php?M=tkcropthumbs_crop&image=' . $fObj['row']['uid_local']
-//			. '&aspectratio=' . $fObj['itemFormElValue']
-//			. '\',\'fenster' . rand(0, 1000000) . ''
-//			. '\',\'height=620,width=820,status=0,menubar=0,scrollbars=0\');return false;">123
-//			<img src="EXT:tkcropthumbs/Resources/Public/Icons/crop.png" width="100%" height="100%">
-//			</a>';
-//		$return .= '<input type="hidden" value="' . $fObj['itemFormElValue'] . '" name="' . $fObj['itemFormElName'] . '" id="zabus_crop_fal_input" />';
-
 		return $formField;
 	}
 }
