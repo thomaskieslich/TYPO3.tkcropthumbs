f440b5025eb7dcb9dd24b17905eba359329d40ac
Thomas Kieslich <info@thomaskieslich.net>
2014-04-02 15:06:54 +0200

    change Ajax register

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,4 +9,9 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tceforms_inline.php
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['getImgResource'][$_EXTKEY] =
 	'ThomasKieslich\\Tkcropthumbs\\Hooks\\CropScaleHook';
 
-$TYPO3_CONF_VARS['BE']['AJAX']['TkcropthumbsAjaxController::init'] = 'ThomasKieslich\\Tkcropthumbs\\Controller\\AjaxController->init';
+if (TYPO3_MODE === 'BE') {
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
+		'TkcropthumbsAjaxController::init',
+		'ThomasKieslich\\Tkcropthumbs\\Controller\\AjaxController->init'
+	);
+}
